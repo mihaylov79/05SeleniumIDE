613f596aeb9856b80ef995ef4cfa448a44009c90
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-06-09 22:32:42 +0300

    Changed SetUp Method

--- SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
+++ SeleniumIDE/TC01IfUserIsInvalidTryAgainTest1.cs
@@ -21,6 +21,12 @@ public class TC01IfUserIsInvalidTryAgainTest
     [SetUp]
     public void SetUp()
     {
+        ChromeOptions options = new ChromeOptions();
+        options.AddArguments("headless");
+        options.AddArguments("no-sandbox");
+        options.AddArguments("disable-gpu");
+        options.AddArguments("window-size=1920x1080");
+        
         driver = new ChromeDriver();
         js = (IJavaScriptExecutor)driver;
         vars = new Dictionary<string, object>();
